e2b618a43d507c872bb1bc9537059758b0b3431d
Emir <emirsagyndykov931@gmail.com>
2022-10-12 00:53:42 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+// let patternPhonKG = /^\+996\s\d{3}\s\d{2}\s\d{2}\s\d{2}/
+// let patternPhonRu = /^\+7\s\d{3}\s\d{3}\s\d{2}\s\d{2}/
+
+// const phoneInput = document.querySelector('#phone');
+// const btn = document.querySelector('button')
+// const countries = document.querySelector('.countries')
+
+// btn.addEventListener('click', e => {
+//     if(patternPhonKG.test(phoneInput.value)){
+//         countries.innerHTML = 'KG'
+//     } else if(patternPhonRu.test(phoneInput.value)){
+//         countries.innerHTML = 'RU'
+//     } else {
+//         alert('ne naiden');
+//     }
+//     })
+      
+
+// let count = 0;
+// function recursion() {
+//     count ++;
+//     console.log(count, 'array');
+//     if(count === 3) {
+//         return
+//     } else {
+//         return recursion()
+//     }
+// }
+// recursion()
+ 
+
+// let arr = [4,5,6]
+// arr.push (7,8,9,10)
+// arr.unshift (1,2,3)
+// console.log(arr);
+
+
+// let arr2 = [1,2,3,4,5]
+// console.log(arr[4]);
+
+
+// function countChar () {
+//     let count = "loremipsumdolor"
+//     console.log(count.match(/o/g));
+// }
+// countChar()
